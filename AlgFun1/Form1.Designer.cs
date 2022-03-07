1afeaa31c78576151cfef80af5791e8bfde909f9
AmetoX <xametox.officiall@yahoo.com>
2022-03-07 10:14:33 +0200

    Curs 1

--- AlgFun1/Form1.Designer.cs
+++ AlgFun1/Form1.Designer.cs
@@ -28,20 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(244, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(244, 23);
+            this.textBox3.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
--- AlgFun1/Form1.cs
+++ AlgFun1/Form1.cs
@@ -6,10 +6,117 @@ namespace AlgFun1
         {
             InitializeComponent();
         }
-
+        Random rnd = new Random();
+        int a, b, c, d, e_;
         private void Form1_Load(object sender, EventArgs e)
         {
+            a = rnd.Next(10);
+            b = rnd.Next(10);
+            c = rnd.Next(10);
+            d = rnd.Next(10);
+            e_ = rnd.Next(10);
 
+            textBox1.Text = a + " " + b + " " + c + " " + d + " " + e_;
+
+            int aux;
+            if (a > b)
+            {
+                aux = a;
+                a = b;
+                b = aux;
+            }
+            if (a > c)
+            {
+                aux = a;
+                a = c;
+                c = aux;
+            }
+            if (a > d)
+            {
+                aux = a;
+                a = d;
+                d = aux;
+            }
+            if (a > e_)
+            {
+                aux = a;
+                a = e_;
+                e_ = aux;
+            }
+            if (b > c)
+            {
+                aux = b;
+                b = c;
+                c = aux;
+            }
+            if (b > d)
+            {
+                aux = b;
+                b = d;
+                d = aux;
+            }
+            if (b > e_)
+            {
+                aux = b;
+                b = e_;
+                e_ = aux;
+            }
+            if (c > d)
+            {
+                aux = c;
+                c = d;
+                d = aux;
+            }
+            if (c > e_)
+            {
+                aux = c;
+                c = e_;
+                e_ = aux;
+            }
+            if (d > e_)
+            {
+                aux = d;
+                d = e_;
+                e_ = aux;
+            }
+            //to do sort
+            //Operatorii relationali <,>, == , != ...
+            //Operatorii logici &&, , ...
+            textBox3.Text = a + " " + b + " " + c + " " + d + " " + e_;
+            if ((a == b) && (a == c) && (a == d) && (a == e_))
+                textBox2.Text = "5 val id.";
+            else
+            {
+                if (((a == b) && (a == c) && (a == d)) || ((b == c) && (b == d) && (b == e_)))
+                    textBox2.Text = "4 val id.";
+                else
+                {
+                    if (((a == b) && (a == c) && (d == e_)) || ((a == b) && (c == d) && (c == e_)))
+                        textBox2.Text = "3 val id. + 2 val id.";
+                    else
+                    {
+                        if (((a == b) && (a == c)) || ((b == c) && (b == d)) || ((c == d) && (c == e_)))
+                            textBox2.Text = "3 val id.";
+                        else
+                        {
+                            //abcde
+                            //00112
+                            //00122
+                            //01122
+                            if (((a == b) && (c == d)) || ((a == b) && (d == e_)) || ((b == c) && (d == e_)))
+                                textBox2.Text = "2 val id. + 2 val id.";
+                            else
+                            {
+                                if ((a == b) || (b == c) || (c == d) || (d == e_))
+                                    textBox2.Text = "2 val id.";
+                                else
+                                    textBox2.Text = "nimic";
+                            }
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
