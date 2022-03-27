7b8ab0ef2dadf21a1a09b0c9c395e6035083e6d6
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-27 12:29:58 +0300

    curs

---
+++ Matrici(Curs)/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Matrici_Curs_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextReader load = new StreamReader(@"C:\Users\Marius\Documents\.C#\AlgoritmiFundamentali\Matrici(Curs)\TextFile1.txt");
+            List<string> data = new List<string>();
+            //data.Add(load.ReadLine());
+            string buffer;
+            while((buffer = load.ReadLine()) != null)
+            {
+                data.Add(buffer);
+            }
+            load.Close();
+
+            foreach(string line in data)
+            {
+                listBox1.Items.Add(line);
+            }
+            int n = int.Parse(data[0].Split(' ')[0]);
+            int m = int.Parse(data[1].Split(' ')[1]);
+            int[,] matrix = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                string[] t = data[i + 1].Split(' ');
+                for(int j = 0;j < m; j++)
+                {
+                    matrix[i,j] = int.Parse(t[j]);
+                }
+            }
+
+            string b;
+            for(int i = 0; i < n; i++)
+            {
+                b = "";
+                for(int j = 0; j < m; j++)
+                {
+                    b += matrix[i, j]+" ";
+                }
+                listBox2.Items.Add(b);
+            }
+        }
+    }
+}
---
+++ SortingApp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SortingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextReader load = new StreamReader(@"C:\Users\Marius\Documents\.C#\AlgoritmiFundamentali\SortingApp\TextFile1.txt");
+            int n = int.Parse(load.ReadLine());
+            int[] v = new int[n];
+            for(int i = 0; i < n-5; i++)
+            {
+                v[i] = int.Parse(load.ReadLine());
+                listBox1.Items.Add(v[i]);
+                //listBox1.Items.Add(load.ReadLine());
+            }
+            string buffer = load.ReadLine();
+            string[] t = buffer.Split(' '); 
+            for(int i = n-5; i < n; i++)
+            {
+                v[i] = int.Parse(t[i-n+5]);
+                listBox1.Items.Add(v[i]);
+            }
+
+            string toV = "";
+            for (int i = 0; i < n; i++)
+            {
+                toV += v[i] + " ";
+
+            }
+            listBox2.Items.Add(toV);
+            toV = "";
+
+            //--Bublle sort--
+            bool ok;
+            int aux;
+            do
+            {
+                ok = true;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (v[i] > v[i + 1])
+                    {
+                        aux = v[i];
+                        v[i] = v[i + 1];
+                        v[i + 1] = aux;
+                        ok = false;
+                    }
+                }
+            } while (!ok);
+            //-----
+
+            for(int i = 0; i < n; i++)
+            {
+                toV += v[i]+" ";
+            }
+            listBox2.Items.Add(toV);
+
+            //--Selection Sort--
+
+            //-----
+        }
+    }
+}
