bb8581a020c01531f0f4477d4ab0dc4f1a866e13
AmetoX <xametox.officiall@yahoo.com>
2022-06-09 21:30:23 +0300

    curs13

---
+++ Curs13_CifreRomane_greedy/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Curs13_CifreRomane_greedy
+{
+    class Program
+    {
+        static int roman(char R)
+        {
+            switch (R)
+            {
+                case 'M': return 1000;
+                case 'D': return 500;
+                case 'C': return 100;
+                case 'L': return 50;
+                case 'X': return 10;
+                case 'V': return 5;
+                case 'I': return 1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            string s = "MMCMXLVII";
+            int t = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (roman(s[i]) >= roman(s[i + 1]))
+                    t += roman(s[i]);
+                else
+                    t -= roman(s[i]);
+            }
+            t += roman(s[s.Length - 1]);
+            Console.Write(t);
+        }
+    }
+}
---
+++ Curs13_Suma/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Curs13_Suma
+{
+    /*Se da o suma(S) si un vector(V) care reprezinta valoarea unor bancnote. 
+Se cere sa se afiseze suma S ca si o combinatie liniara de elemente din V.
+Conditie: Suma coeficientilor sa fie minima.
+S = 2917 
+V = {100, 500, 100, 20, 5, 1}
+2917 x 1*/
+    class Program
+    {
+        static int s = 2917;
+        static int[] v = new int[] { 1000, 500, 100, 20, 5, 1 };
+        static void Main(string[] args)
+        {
+            int[] r = new int[v.Length];
+            for (int i = 0; i < v.Length; i++)
+            {
+                r[i] = s / v[i];
+                s = s % v[i];
+            }
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write(r[i] + "*" + v[i] + "+");
+            }
+        }
+    }
+}
